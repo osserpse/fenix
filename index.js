c5cd2289c04053899c2c3244a09a2d7a75c09701
oa <lars.olof.berg@happyuser.se>
2017-12-15 22:30:26 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+
+// import './assets/css/foundation-sites.css';
+// import './assets/css/app.css';
+// import './assets/css/fonts.css';
+// import './assets/fonts/iconfa.css';
+// import './assets/fonts/flag/flag-icon.css';
